23465c69bb20cd0e386c3881f3008398835444f1
NonorCoding <aurele.joblet@gmail.com>
2024-08-31 02:06:05 +0200

    Add score board and fix some bugs

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,22 +1,41 @@
-import { useState } from "react";
+import {usePlayerContext} from "../context/PlayerContext.jsx";
+import {gameSettings, stats, useGameContext} from "../context/GameContext.jsx";
 
 export const UI = () => {
-  const [win, setWin] = useState(false);
+    const {getHitCount, getStrength} = usePlayerContext();
+    const {gameState, nextHole, currentHole} = useGameContext();
 
-  return (
-    <>
+    return (<>
 
-      {/* Force gauge */}
+        <ul className="score">
+            <li>
+                Hits : {getHitCount()}
+            </li>
+            <li>
+                Par: {gameSettings[currentHole].par}
+            </li>
+            <li>
+                Hole: {currentHole + 1}
+            </li>
+            <li>
+                Game state: {gameState}
+            </li>
+        </ul>
 
-      <div className="force-gauge">
-        <div className="force-bar"></div>
-      </div>
-
-      {win && (
-        <div className="win">
-          WIN
+        <div className="force-gauge">
+            <div
+                className="force-bar"
+                style={{
+                    clipPath: `polygon(0 ${100 - ((getStrength() - 0.0005) / (0.002 - 0.0005)) * 100}%, 100% ${100 - ((getStrength() - 0.0005) / (0.002 - 0.0005)) * 100}%, 100% 100%, 0 100%)`,
+                }}
+            />
         </div>
-      )}
-    </>
-  );
+
+        {gameState === stats.win && (<div className="win">
+            WIN
+
+            {/*Next */}
+            <button onClick={() => nextHole()}>Next</button>
+        </div>)}
+    </>);
 };
